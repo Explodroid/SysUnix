b005be557d400567c5086751813475e2aedd21c0
Renaud de Ganay <renaud.deganay@epitech.eu>
2015-11-11 23:08:03 +0100

    backn correction : now adding 1 to the correct int

--- Printf/printf_backn.c
+++ Printf/printf_backn.c
@@ -5,6 +5,6 @@ void		printf_backn(t_print *print, char *format)
   if (format[print->idx + 1] == '\n')
     {
       my_putchar('\n');
-      print->idx = print->idx + 1;
+      print->returnv = print->returnv + 1;
     }
 }
